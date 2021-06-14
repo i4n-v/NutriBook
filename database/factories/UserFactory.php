d3192b945f40aa09fa80d8d1bdeadae2aba7eea3
i4n-v <ianvinicioss@gmail.com>
2021-06-14 17:22:02 -0300

    GH-15 Adding model, factory and migratiom for nutricionist

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class UserFactory extends Factory
 {
@@ -24,9 +25,11 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'CPF' => $this->faker->unique()->randomNumber(11, true),
+            'CRN' => $this->faker->unique()->randomNumber(4, true),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('12345678'), // password
             'remember_token' => Str::random(10),
         ];
     }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('nutricionist', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->integer('CPF')->unique();
+            $table->integer('CRN')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
