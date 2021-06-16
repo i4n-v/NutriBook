0e835c02a07276eff7a48bf5a8611591de2b903d
bismarckcesar <bismarcksport@gmail.com>
2021-06-16 17:43:59 -0300

    GH-27 Adding controller, model, factory and migratiom for Meal

---
+++ database/factories/MealFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Meal;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class MealFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Meal::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'day'=> $this->faker->dayOfWeek(),
+            'day_period'=> $this->faker->words(),
+        ];
+    }
+}
