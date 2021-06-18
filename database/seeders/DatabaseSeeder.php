ee2682ef58f3d636f2931eb952cc0686c1674f86
i4n-v <ianvinicioss@gmail.com>
2021-06-18 00:04:37 -0300

    GH-15 Implementation of the nutritionist entity's seeders and relationships

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\Nutricionist::factory(10)->create();
+        $this->call([
+            NutritionistSeeder::class,
+            PatientSeeder::class,
+            EvaluationSeeder::class,
+        ]);
     }
 }
