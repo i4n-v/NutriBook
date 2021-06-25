fcdc5737942e067cf5b5882fed09acc9e8c036a1
i4n-v <ianvinicioss@gmail.com>
2021-06-25 16:20:34 -0300

    GH-32 and GH-33 search bar for home page

---
+++ resources/views/components/search-bar.blade.php
@@ -0,0 +1,7 @@
+<div class="w-4/12 h-1/6 relative">
+    <x-input class="w-full h-full" type="text" placeholder="{{ $placeholder }}"/>
+    
+    <div class="absolute top-1 right-2 cursor-pointer border p-1 rounded-lg bg-yellow-400 transition delay-150 hover:bg-yellow-500">   
+        <x-search-icon/>
+    </div>
+</div>
--- resources/views/layouts/navigation-home.blade.php
+++ resources/views/layouts/navigation-home.blade.php
@@ -1,5 +1,5 @@
-<nav class="bg-gray-900 text-white min-w-screen border-b"> 
-    <div class="flex justify-between w-full h-16 mx-auto px-4 sm:px-6 lg:px-8">
+<nav class="bg-gray-900 text-white min-w-screen border-b relative" x-data="{click:false }"> 
+    <div class="flex justify-between w-full h-16 mx-auto px-4 sm:px-6 lg:px-8 static">
         <div div class="flex-initial flex w-1/2 items-center">
             <div>
                 <a href="{{ route('login') }}">
@@ -12,8 +12,13 @@
         </div>
 
         <div class="flex-initial w-1/6 flex items-center float-right gap-8">
-            <a class="border-b-2 border-gray-900 transition delay-150 hover:border-yellow-300" href="{{ route('login') }}">Entrar</a> 
-            <a class="border-b-2 border-gray-900 transition delay-150 hover:border-yellow-300" href="{{ route('register') }}">Cadastrar-se</a>
+            @if (!Auth::user()!=null)
+                <a class="border-b-2 border-gray-900 transition delay-150 hover:border-yellow-300" href="{{ route('login') }}">Entrar</a> 
+                <a class="border-b-2 border-gray-900 transition delay-150 hover:border-yellow-300" href="{{ route('register') }}">Cadastrar-se</a>
+            @else
+                <x-nav-link/>
+            @endif
         </div>
     </div>
+    <x-dropdown/>
 </nav>
