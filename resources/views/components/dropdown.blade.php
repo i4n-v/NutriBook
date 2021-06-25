7e60e3c00476e8b7048089b9aba3051a6c0f7f7c
i4n-v <ianvinicioss@gmail.com>
2021-06-25 16:19:20 -0300

    GH-32 and GH-33 adding dropdown to Home

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,43 +1,8 @@
-@props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white'])
+<div x-show="click" class="grid grid-rows-2 my-1 divide-y bg-white text-gray-900 absolute left-100 top-100 w-1/6 text-center font-semibold right-7 top-12 rounded-lg shadow-2xl">
+    <a href="#" class="hover:bg-yellow-400 rounded-md py-1">Perfil</a>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
 
-@php
-switch ($align) {
-    case 'left':
-        $alignmentClasses = 'origin-top-left left-0';
-        break;
-    case 'top':
-        $alignmentClasses = 'origin-top';
-        break;
-    case 'right':
-    default:
-        $alignmentClasses = 'origin-top-right right-0';
-        break;
-}
-
-switch ($width) {
-    case '48':
-        $width = 'w-48';
-        break;
-}
-@endphp
-
-<div class="relative" x-data="{ open: false }" @click.away="open = false" @close.stop="open = false">
-    <div @click="open = ! open">
-        {{ $trigger }}
-    </div>
-
-    <div x-show="open"
-            x-transition:enter="transition ease-out duration-200"
-            x-transition:enter-start="transform opacity-0 scale-95"
-            x-transition:enter-end="transform opacity-100 scale-100"
-            x-transition:leave="transition ease-in duration-75"
-            x-transition:leave-start="transform opacity-100 scale-100"
-            x-transition:leave-end="transform opacity-0 scale-95"
-            class="absolute z-50 mt-2 {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
-            style="display: none;"
-            @click="open = false">
-        <div class="rounded-md ring-1 ring-black ring-opacity-5 {{ $contentClasses }}">
-            {{ $content }}
-        </div>
-    </div>
+        <button type='submit', class="hover:bg-yellow-400 rounded-md py-1 w-full h-full focus:outline-none">Sair</button>
+    </form>
 </div>
--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,10 @@
-@props(['active'])
-
-@php
-$classes = ($active ?? false)
-            ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
-@endphp
-
-<a {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
-</a>
+<div class="flex items-center justify-center">
+    <div class="mr-2">
+        <img class="cursor-pointer" class="pl-1" src="{{ asset('img/icons/bond.svg') }}" alt="dropdown icon">
+    </div>
+    <div class="cursor-pointer w-full h-1/2" @click="click = !click">
+        <span class="transition delay-150 border-b-2 border-white hover:border-yellow-300" x-bind:class="click?'border-yellow-300':''">
+            {{ Auth::user()->name }}
+        </span>
+    </div>
+</div>
