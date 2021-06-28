e82af1fe6040c0971c686ccf3f14c46ed60ed729
alowce <alicebrandt.c@gmal.com>
2021-06-28 00:15:17 -0300

    GH-32 Adding route to home view

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,8 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
+
 require __DIR__.'/auth.php';
