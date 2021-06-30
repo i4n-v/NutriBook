5257277b79403b8639d1339d1fda0b60a0b065bd
i4n-v <ianvinicioss@gmail.com>
2021-06-30 18:20:38 -0300

    GH-40 and GH-39 -Adding visual validation of the food register

---
+++ resources/views/components/message.blade.php
@@ -0,0 +1,11 @@
+@props(['sucess', 'error'])
+
+@if($sucess!='')
+    <div class="mx-auto mb-5 bg-green-300 p-1 w-9/12 rounded-md flex justify-center items-center">
+        <span class="font-semibold">{{ $sucess }}</span>
+    </div>
+@else
+    <div class="mx-auto mb-5 bg-red-300 p-1 w-9/12 rounded-md flex justify-center items-center">
+        <span class="font-semibold">{{ $error }}</span>
+    </div>
+@endif
