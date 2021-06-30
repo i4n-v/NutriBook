c0682bfe6672905c43f5808b09ed7af970cfeb32
i4n-v <ianvinicioss@gmail.com>
2021-06-30 17:58:11 -0300

    GH-39 -Adding Page-Related Button Components

---
+++ resources/views/components/back-button.blade.php
@@ -0,0 +1,5 @@
+<div onclick="window.history.back()" class="h-8 w-8 bg-yellow-400 rounded-lg flex items-center justify-center cursor-pointer transitions delay-150 hover:bg-yellow-500 -mt-1">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+    </svg>
+</div>
