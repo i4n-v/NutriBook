d419f3d20c02b63a2542de7aa4b5f825c59c4b88
alowce <alicebrandt.c@gmail.com>
2021-06-29 22:11:58 -0300

    GH-32 Renaming patient ordering component

---
+++ resources/views/components/ordering-patients.blade.php
@@ -0,0 +1,19 @@
+<div class="font-bold">
+    Ordenar por:
+    <div class="flex pt-2">
+        <input id="filter-1" class="border-solid border border-gray-900 rounded mt-0.5" type="radio" name="filter">
+        <label for="filter-1" class="pl-1.5">A-Z</label>
+    </div>
+    <div class="flex">
+        <input id="filter-2" class="border-solid border border-gray-900 rounded mt-1" type="radio" name="filter">
+        <label for="filter-2" class="pl-1.5 mt-0.5">Z-A</label>
+    </div>
+    <div class="flex">
+        <input id="filter-3" class="border-solid border border-gray-900 rounded mt-1" type="radio" name="filter">
+        <label for="filter-3" class="pl-1.5 mb-0.5">Vínculos mais recentes</label>
+    </div>
+    <div class="flex">
+        <input id="filter-4" class="border-solid border border-gray-900 rounded mt-0.5" type="radio" name="filter">
+        <label for="filter-4" class="pl-1.5">Vínculos mais antigos</label>
+    </div>
+</div>
