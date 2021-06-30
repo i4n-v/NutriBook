937cf1be5c8b3b37a613e5bf1b9867cdfea7f9c1
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-06-30 01:39:04 -0300

    GH-32 Listing patients from the nutritionist

--- resources/views/components/table-my-patients.blade.php
+++ resources/views/components/table-my-patients.blade.php
@@ -1,3 +1,9 @@
+@php
+
+$nutritionist_collection = App\Models\Nutritionist::where('user_id', Auth::user()->id)->get();
+
+@endphp
+
 <table class="w-full bg-white shadow-md border-solid border-2 border-gray-200">
     <thead class="h-10 bg-gray-200 text-left">
         <th class="pl-1">Nome</th>
@@ -5,21 +11,34 @@
         <th>Data de vínculo</th>
         <th class="text-center">Perfil</th>
     </thead>
-    <tr class="h-10 hover:bg-gray-100 border-solid border border-gray-200">
-        <td class="pl-1">
-            Joselindo Bonifácio Correia Neto
-        </td>
-        <td class="text-left">
-            114.225.439-85
-        </td>
-        <td class="text-left">
-            28/06/2021
-        </td>
-        <td class="text-center">
-            </button>
-            <x-button>
-                {{ __('Acessar perfil') }}
-            </x-button>
-        </td>
-    </tr>
+
+    @foreach ($nutritionist_collection as $nutritionist)
+        @foreach ($nutritionist->patient as $patient)
+            @php
+            $user_collection = App\Models\User::where('id', $patient->user_id)->get();
+            @endphp
+
+            @foreach ($user_collection as $user) 
+            <tr class="h-10 hover:bg-gray-100 border-solid border border-gray-200">
+                <td class="pl-1">
+                    {{ $user->name }}
+                </td>
+                <td class="text-left">
+                    {{ $user->CPF }}
+                </td>
+                <td class="text-left">
+                    28/06/2021 (Em breve)
+                </td>
+                <td class="text-center">
+                    </button>
+                    <x-button>
+                        {{ __('Acessar perfil') }}
+                    </x-button>
+                </td>
+            </tr>
+            @endforeach
+
+        @endforeach
+    @endforeach
+
 </table>
