53f9ef601177741b5ae369f3e0062be31b2ef5e0
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-06-30 16:36:14 -0300

    GH-33 Listing eating plans from the patient

--- resources/views/components/table-my-eating-plans.blade.php
+++ resources/views/components/table-my-eating-plans.blade.php
@@ -1,3 +1,9 @@
+@php
+
+$patient_collection = App\Models\Patient::where('user_id', Auth::user()->id)->get(); 
+
+@endphp
+
 <table class="w-full bg-white shadow-md border-solid border-2 border-gray-200">
         <thead class="h-10 bg-gray-200 text-left">
             <th class="pl-1">Título</th>
@@ -5,26 +11,37 @@
             <th>Data de término</th>
             <th class="text-center">Ações</th>
         </thead>
-        <tr class="h-10 hover:bg-gray-100 border-solid border border-gray-200">
-            <td class="pl-1">
-                Queimando calorias
-            </td>
-            <td class="text-left">
-                10/05/2021
-            </td>
-            <td class="text-left">
-                28/06/2021
-            </td>
-            <td class="text-center">
-                <x-button-delete>
-                    {{ ('Apagar') }}
-                </x-button-delete>
-                <x-button-edit>
-                    {{ ('Editar') }}
-                </x-button-edit>
-                <x-button>
-                    {{ __('Acessar') }}
-                </x-button>
-            </td>
-        </tr>
+
+        @foreach ($patient_collection as $patient)
+            @php
+            $eating_plan_collection = App\Models\EatingPlan::where('patient_id', $patient->id)->get();
+            @endphp
+
+            @foreach ($eating_plan_collection as $eating_plan)
+            <tr class="h-10 hover:bg-gray-100 border-solid border border-gray-200">
+                <td class="pl-1">
+                    {{ $eating_plan->title }}
+                </td>
+                <td class="text-left">
+                    {{ $eating_plan->date_start }}
+                </td>
+                <td class="text-left">
+                    {{ $eating_plan->date_finish }}
+                </td>
+                <td class="text-center">
+                    <x-button-delete>
+                        {{ ('Apagar') }}
+                    </x-button-delete>
+                    <x-button-edit>
+                        {{ ('Editar') }}
+                    </x-button-edit>
+                    <x-button>
+                        {{ __('Acessar') }}
+                    </x-button>
+                </td>
+            </tr>
+            @endforeach
+
+        @endforeach
+
  </table>
