8786df5c22d3f068684e6a4c62929cde63e2ff1b
alowce <alicebrandt.c@gmail.com>
2021-07-06 17:38:34 -0300

    GH-32 and GH-33 Adding ordering selector component to tables

---
+++ resources/views/components/ordering-selector.blade.php
@@ -0,0 +1,17 @@
+<div class="grid grid-cols-12">
+    <div class="col-start-1 col-end-10 m-auto">
+        {{ $slot }}
+    </div>
+    <div class="col-start-11 col-end-12">
+        <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
+            </svg>
+        </a>
+        <a href="">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+            </svg>
+        </a>
+    </div>
+</div>
