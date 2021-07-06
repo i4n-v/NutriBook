3e3e093464f4cc2e78abaf2955ea04dd16aa523b
bismarckcesar <bismarcksport@gmail.com>
2021-07-06 18:26:11 -0300

    GH-39 changing the look of the buttons

--- resources/views/components/button-delete.blade.php
+++ resources/views/components/button-delete.blade.php
@@ -1,5 +1,5 @@
-<a {{ $attributes->merge(['title'=>'Excluir', 'class' => 'cursor-pointer inline-flex items-center px-2 py-1 bg-red-400 border border-transparent rounded-md font-bold text-xs text-gray-900 uppercase tracking-widest hover:bg-red-500 active:bg-gray-900 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<a {{ $attributes->merge(['title'=>'Excluir', 'class' => 'cursor-pointer inline-flex items-center px-2 py-1  border border-transparent rounded-md font-bold text-xs text-gray-900 uppercase tracking-widest hover:bg-red-100 active:bg-gray-900 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-red-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
     </svg>
 </a>
--- resources/views/components/button-edit.blade.php
+++ resources/views/components/button-edit.blade.php
@@ -1,5 +1,5 @@
-<a {{ $attributes->merge(['title'=>'Editar', 'class' => 'cursor-pointer inline-flex items-center px-2 py-1 bg-green-400 border border-transparent rounded-md font-bold text-xs text-gray-900 uppercase tracking-widest hover:bg-green-500 active:bg-gray-900 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
-    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-900" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<a {{ $attributes->merge(['title'=>'Editar', 'class' => 'cursor-pointer inline-flex items-center px-2 py-1  border border-transparent rounded-md font-bold text-xs text-gray-900 uppercase tracking-widest hover:bg-green-100 active:bg-gray-900 focus:outline-none focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-700" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
     </svg>
 </a>
