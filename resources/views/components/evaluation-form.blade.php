109721691b2798d57cf7cbdcca8b1e5be86b1086
i4n-v <ianvinicioss@gmail.com>
2021-07-08 21:00:43 -0300

    GH-46 -Ending evaluation registration page

--- resources/views/components/evaluation-form.blade.php
+++ resources/views/components/evaluation-form.blade.php
@@ -1,32 +1,26 @@
-<form method="POST" action="{{ route('create_evaluation')}}">
-	@csrf
-	<fieldset>
-		<legend class="text-white text-2xl border-b-2 mb-5">Avaliação</legend>
-	
-            <!-- 'weight' -->
-             <div>
-            <x-label for="weight" :value="__('Peso do Paciente')"/>
-            <x-input id="weight" class="block mt-1 w-full" type="text" name="weight" required autofocus placeholder="Digite o peso do Paciente "/>
-           </div>
+<form method="POST" action="{{ route('create_evaluation')}}" x-show="ava">
+    @csrf
+    <fieldset>
+        <legend class="text-white text-2xl border-b-2 mb-5">Avaliação</legend>
 
-            <!-- 'height' -->
-            <div>
-            <x-label for="height" :value="__('Altura do Paciente')"/>
-            <x-input id="height" class="block mt-1 w-full" type="text" name="height"  required autofocus placeholder="Digite a altura do Paciente "/>
-             </div>
-        
+        <!-- 'weight' -->
+        <div>
+            <x-label for="weight" :value="__('Peso do Paciente')" />
+            <x-input id="weight" class="block mt-1 w-full" type="number" name="weight" required autofocus placeholder="Peso do Paciente " />
+        </div>
 
-        <div class="flex items-center justify-end mt-4">
-                <x-cancel-button>
-                    Cancelar
-                </x-cancel-button>
+        <!-- 'height' -->
+        <div>
+            <x-label for="height" :value="__('Altura do Paciente')" />
+            <x-input id="height" class="block mt-1 w-full" type="number" name="height" required autofocus placeholder="Altura do Paciente " />
+        </div>
 
-                <x-button class="ml-4">
-                    {{ __('Criar') }}
-                </x-button>
+    </fieldset>
 
-             </div>
+    <div class="flex items-center justify-end mt-4">
+        <x-button class="ml-4">
+            {{ __('Salvar') }}
+        </x-button>
+    </div>
 
-
-	</fieldset>
 </form>
--- resources/views/evaluation.blade.php
+++ resources/views/evaluation.blade.php
@@ -1,38 +1,39 @@
 <x-guest-layout>
-
-	<div class="min-h-screen bg-white" x-data="{ food:true }">
-        <x-slot name="header"> 
+    <div class="min-h-screen bg-white">
+        <x-slot name="header">
             <h2 class="font-semibold text-2xl text-gray-900 leading-tight">
                 {{ __('Crie avaliações para seus pacientes!') }}
             </h2>
         </x-slot>
-		
-
-       <div class="py-12">
+        <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg" id="divBody">
-                        <div class="p-6 bg-gray-100 border-b border-gray-200 bg-gray-800 w-10/12 rounded-lg m-auto">
-                            <div class="flex gap-4">
-                               
-                               <div class="p-6 border-gray-200 bg-gray-800 w-10/12 rounded-lg m-auto">
-                                    <div class="flex gap-4" x-data="{ food=true }">
-                                <x-form-create-layout>
-                                    <x-anamnese-form/>
-                                </x-form-create-layout>
-                          
-                                                           
-                       </div>
-                   </div>  
-               </div>
+                    <div class="p-1 border-b border-gray-200 bg-gray-800 rounded-lg m-auto w-10/12">
+                        <div class="flex flex-wrap gap-4 w-full h-full" x-data="{ ava:true, med:false, dob:false, ana:false }">
+
+                            <div class="bg-white rounded-md">
+                                <div class="grid grid-rows-4 text-center p-1">
+                                    <span class="w-full py-2 px-2 border-b transition delay-150 hover:bg-yellow-400 cursor-pointer rounded-md" @click="ava=true; med=false; dob=false; ana=false" x-bind:class="ava?'bg-yellow-400':''">Avaliação</span>
+                                    <span class="w-full py-2 px-2 border-b transition delay-150 hover:bg-yellow-400 cursor-pointer rounded-md" @click="ava=false; med=true; dob=false; ana=false" x-bind:class="med?'bg-yellow-400':''">Medidas corporais</span>
+                                    <span class="w-full py-2 px-2 border-b transition delay-150 hover:bg-yellow-400 cursor-pointer rounded-md" @click="ava=false; med=false; dob=true; ana=false" x-bind:class="dob?'bg-yellow-400':''">Dobras cutâneas</span>
+                                    <span class="w-full py-2 px-2 border-b transition delay-150 hover:bg-yellow-400 cursor-pointer rounded-md" @click="ava=false; med=false; dob=false; ana=true" x-bind:class="ana?'bg-yellow-400':''">Anamnese</span>
+                                    <span class="w-full py-2 px-2 border-b transition delay-150 hover:bg-yellow-400 cursor-pointer rounded-md">Voltar</span>
+                                </div>
+                            </div>
+
+                            <div class="ml-auto w-9/12">
+                                <div class="mx-auto w-7/12 p-5">
+                                    <x-evaluation-form />
+                                    <x-body-mean-form />
+                                    <x-skinfold-form />
+                                    <x-anamnese-form />
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
-</div>
-
-<div>
-
-</div>
-
-
+        </div>
+    </div>
 </x-guest-layout>
