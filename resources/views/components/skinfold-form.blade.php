78f81615192dabdf1630657f3a5493dc4b462544
i4n-v <ianvinicioss@gmail.com>
2021-07-08 21:07:40 -0300

    GH-42 -Adjusting the form of skinfolds

--- resources/views/components/skinfold-form.blade.php
+++ resources/views/components/skinfold-form.blade.php
@@ -1,94 +1,86 @@
-<form method="POST" action="{{ route('create_skinfold')}}">
+<form method="POST" action="{{ route('create_skinfold')}}" x-show="dob">
 	@csrf
 	<fieldset>
 		<legend class="text-white text-2xl border-b-2 mb-5">Dobras Cutâneas</legend>
 	
-            
-         <!-- breastplate -->
 
-             <div>
-            <x-label for="breastplate" :value="__('Peitoral')"/>
-            <x-input id="breastplate" class="block mt-1 w-full" type="text" name="breastplate" required autofocus placeholder="Digite o tamanho de Peitoral do paciente"/>
-           </div>
+        <div class="grid grid-cols-2 gap-5 w-full">    
+            <!-- breastplate -->
 
-         <!-- biceps -->
+            <div>
+                <x-label for="breastplate" :value="__('Peitoral')"/>
+                <x-input id="breastplate" class="block mt-1 w-full" type="number" name="breastplate" required autofocus placeholder="Medida do Peitoral"/>
+            </div>
+
+            <!-- biceps -->
         
             <div>
-            <x-label for="biceps" :value="__('Bíceps')"/>
-            <x-input id="biceps" class="block mt-1 w-full" type="text" name="biceps"  required autofocus placeholder="Digite o tamanho dos Bíceps do paciente"/>
-             </div>
+                <x-label for="biceps" :value="__('Bíceps')"/>
+                <x-input id="biceps" class="block mt-1 w-full" type="number" name="biceps"  required autofocus placeholder="Medida dos Bíceps"/>
+            </div>
 
 
-         <!-- triceps -->
-
-            <div>
-            <x-label for="triceps" :value="__('Trícep')"/>
-            <x-input id="triceps" class="block mt-1 w-full" type="text" name="triceps"  required autofocus placeholder="Digite o tamanho dos Tríceps do paciente"/>
-             </div>
-
-         <!-- abdominal -->
-
-             <div>
-            <x-label for="abdominal" :value="__('Abdômen')"/>
-            <x-input id="abdominal" class="block mt-1 w-full" type="text" name="abdominal"  required autofocus placeholder="Digite a medida de Abdômen do paciente"/>
-             </div>
-
-         <!--subscapular -->
+            <!-- triceps -->
 
             <div>
-            <x-label for="subscapular" :value="__('Subescapular')"/>
-            <x-input id="subscapular" class="block mt-1 w-full" type="text" name="subscapular"  required autofocus placeholder="Digite o tamanho de Subescapular do paciente"/>
-             </div>
+                <x-label for="triceps" :value="__('Trícep')"/>
+                <x-input id="triceps" class="block mt-1 w-full" type="number" name="triceps"  required autofocus placeholder="Medida do Tríceps"/>
+            </div>
 
-         <!-- suprailiaco -->
+            <!-- abdominal -->
 
-             <div>
-            <x-label for="suprailiaco" :value="__('Suprailíaco')"/>
-            <x-input id="suprailiaco" class="block mt-1 w-full" type="text" name="suprailiaco"  required autofocus placeholder="Digite a medida de Suprailíaco do paciente"/>
-             </div>
+            <div>
+                <x-label for="abdominal" :value="__('Abdômen')"/>
+                <x-input id="abdominal" class="block mt-1 w-full" type="number" name="abdominal"  required autofocus placeholder="Medida do Abdômen"/>
+            </div>
 
-         <!-- middle_axillary -->
+            <!--subscapular -->
 
-             <div>
-            <x-label for="middle_axillary" :value="__('Auxíliar Médio')"/>
-            <x-input id="middle_axillary" class="block mt-1 w-full" type="text" name="middle_axillary"  required autofocus placeholder="Digite o tamanho de Auxiliar Médio do paciente"/>
-             </div>
+            <div>
+                <x-label for="subscapular" :value="__('Subescapular')"/>
+                <x-input id="subscapular" class="block mt-1 w-full" type="number" name="subscapular"  required autofocus placeholder="Medida da Subescapular"/>
+            </div>
 
-         <!--thigh -->
+            <!-- suprailiaco -->
 
-             <div>
-            <x-label for="thigh" :value="__('Coxa')"/>
-            <x-input id="thigh" class="block mt-1 w-full" type="text" name="thigh"  required autofocus placeholder="Digite a medida de Coxa do paciente"/>
-             </div>
+            <div>
+                <x-label for="suprailiaco" :value="__('Suprailíaco')"/>
+                <x-input id="suprailiaco" class="block mt-1 w-full" type="number" name="suprailiaco"  required autofocus placeholder="Medida do Suprailíaco"/>
+            </div>
 
-          <!--calf -->
+            <!-- middle_axillary -->
 
-          <div>
-            <x-label for="calf" :value="__('Panturrilha')"/>
-            <x-input id="calf" class="block mt-1 w-full" type="text" name="calf"  required autofocus placeholder="Digite a medida de Panturrilha do paciente"/>
-             </div>
+            <div>
+                <x-label for="middle_axillary" :value="__('Axíliar Médio')"/>
+                <x-input id="middle_axillary" class="block mt-1 w-full" type="number" name="middle_axillary"  required autofocus placeholder="Medida do Axiliar Médio"/>
+            </div>
 
-         <!-- Lumbar -->
+            <!--thigh -->
 
-             <div>
-            <x-label for="lumbar" :value="__('Lombar')"/>
-            <x-input id="lumbar" class="block mt-1 w-full" type="text" name="lumbar"  required autofocus placeholder="Digite o tamanho da Lombar do paciente"/>
-             </div>
+            <div>
+                <x-label for="thigh" :value="__('Coxa')"/>
+                <x-input id="thigh" class="block mt-1 w-full" type="number" name="thigh"  required autofocus placeholder="Medida da Coxa"/>
+            </div>
 
+            <!--calf -->
 
-        
+            <div>
+                <x-label for="calf" :value="__('Panturrilha')"/>
+                <x-input id="calf" class="block mt-1 w-full" type="number" name="calf"  required autofocus placeholder="Medida da Panturrilha"/>
+            </div>
 
-        <div class="flex items-center justify-end mt-4">
-                <x-cancel-button>
-                    Cancelar
-                </x-cancel-button>
+            <!-- Lumbar -->
 
-                <x-button class="ml-4">
-                    {{ __('Criar') }}
-                </x-button>
-
-             </div>
-
-
+            <div>
+                <x-label for="lumbar" :value="__('Lombar')"/>
+                <x-input id="lumbar" class="block mt-1 w-full" type="number" name="lumbar"  required autofocus placeholder="Medida da Lombar"/>
+            </div>
+        </div>
 	</fieldset>
+    
+        <div class="flex items-center justify-end mt-4">
+            <x-button class="ml-4">
+                {{ __('Salvar') }}
+            </x-button>
+        </div>
 </form>
