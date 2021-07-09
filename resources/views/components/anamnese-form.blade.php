9cab5ac6c7fe599edb664841d18a3c2429be2991
i4n-v <ianvinicioss@gmail.com>
2021-07-08 21:05:24 -0300

    GH-44 -Adjusting the anamnesis form

--- resources/views/components/anamnese-form.blade.php
+++ resources/views/components/anamnese-form.blade.php
@@ -1,68 +1,48 @@
-<form method="POST" action="{{ route('create_anamnese')}}">
-        @csrf
-        <fieldset>
-                <legend class="text-white text-2xl border-b-2 mb-5">Anamnese</legend>
-        
-            
-            <!--objective -->
+<form method="POST" action="{{ route('create_anamnese')}}" x-show="ana">
+    @csrf
+    <fieldset>
+        <legend class="text-white text-2xl border-b-2 mb-5">Anamnese</legend>
 
+        <!--objective -->
 
-             <div>
-            <x-label for="objective" :value="__('Objetivo')"/>
-            <x-input id="objective" class="block mt-1 w-full" type="text" name="objective" required autofocus placeholder="Digite o Objetivo do paciente"/>
-           </div>
+        <div>
+            <x-label for="objective" :value="__('Objetivo')" />
+            <x-input id="objective" class="block mt-1 w-full" type="text" name="objective" required autofocus placeholder="Objetivo do paciente" />
+        </div>
 
+        <!--pathological_history -->
 
-           <!--pathological_history -->
-
-
-             <div>
-            <x-label for="pathological_history" :value="__('Histórico Patológico')"/>
-            <x-input id="pathological_history" class="block mt-1 w-full" type="text" name="pathological_history" required autofocus placeholder="Digite o Histórico Patológico do paciente"/>
-           </div>
-
+        <div>
+            <x-label for="pathological_history" :value="__('Histórico Patológico')" />
+            <x-input id="pathological_history" class="block mt-1 w-full" type="text" name="pathological_history" required autofocus placeholder="Histórico Patológico do paciente" />
+        </div>
 
         <!--family_history -->
 
+        <div>
+            <x-label for="family_history" :value="__('Histórico Familiar')" />
+            <x-input id="family_history" class="block mt-1 w-full" type="text" name="family_history" required autofocus placeholder="Histórico familiar do paciente" />
+        </div>
 
-             <div>
-            <x-label for="family_history" :value="__('Histórico Familiar')"/>
-            <x-input id="family_history" class="block mt-1 w-full" type="text" name="family_history" required autofocus placeholder="Digite o  do paciente"/>
-           </div>
+        <!--used_drugs -->
 
-          <!--used_drugs -->
+        <div>
+            <x-label for="used_drugs" :value="__('Uso de Remédios')" />
+            <x-input id="used_drugs" class="block mt-1 w-full" type="text" name="used_drugs" required autofocus placeholder="Rémedios utilizados pelo paciente" />
+        </div>
 
+        <!--life_style -->
 
-             <div>
-            <x-label for="used_drugs" :value="__('Uso de Remédios')"/>
-            <x-input id="used_drugs" class="block mt-1 w-full" type="text" name="used_drugs" required autofocus placeholder="Digite se o Paciente usa algum Remédio"/>
-           </div>
+        <div>
+            <x-label for="life_style" :value="__('Estilo de Vida')" />
+            <x-input id="life_style" class="block mt-1 w-full" type="text" name="life_style" required autofocus placeholder="Estilo de Vida do paciente" />
+        </div>
+    </fieldset>
 
+    <div class="flex items-center justify-end mt-4">
+        <x-button class="ml-4">
+            {{ __('Salvar') }}
+        </x-button>
+    </div>
 
-          <!--life_style -->
-
-
-             <div>
-            <x-label for="life_style" :value="__('Estilo de Vida')"/>
-            <x-input id="life_style" class="block mt-1 w-full" type="text" name="life_style" required autofocus placeholder="Digite o Estilo de Vida do paciente"/>
-           </div>
-
-
-
-
-
-
-          <div class="flex items-center justify-end mt-4">
-                <x-cancel-button>
-                    Cancelar
-                </x-cancel-button>
-
-                <x-button class="ml-4">
-                    {{ __('Criar') }}
-                </x-button>
-
-             </div>
-
-
-        </fieldset>
 </form>
