e0fd050e6819fa6aaf2d3d58e6d08af9cf378b85
i4n-v <ianvinicioss@gmail.com>
2021-07-10 15:40:19 -0300

    GH-41 -adding query by patient evaluationsadding query by patient evaluations

--- resources/views/components/table-evaluations.blade.php
+++ resources/views/components/table-evaluations.blade.php
@@ -1,26 +1,15 @@
 @php
-
-if(Auth::user()->isPatient()){
-    $evaluations = App\Models\Evaluation::where('patient_id', Auth::user()->profile()->id)->get();
-}else{
-    $evaluations = App\Models\Evaluation::where('patient_id', App\Models\User::find($_GET['patient'])->profile()->id)->get();
-}
-
+    if(Auth::user()->isPatient()){
+        $evaluations = App\Models\Evaluation::where('patient_id', Auth::user()->profile()->id)->get();
+        $id = Auth::user()->profile()->id;
+    }else{
+        $evaluations = App\Models\Evaluation::where('nutritionist_id', Auth::user()->profile()->id)->where('patient_id', App\Models\User::find($_GET['patient'])->profile()->id)->get();
+        $id = App\Models\User::find($_GET['patient'])->profile()->id;
+    }
 @endphp
 
 <table class="w-full bg-white shadow-lg border-solid border-b-2 rounded-sm">
     <thead class="h-10 bg-gray-700 text-white rounded-sm">
-        <th class="rounded-tl-sm">
-            @if(Auth::user()->isPatient())
-            <x-ordering-selector>
-                {{ __('Nutricionista') }}
-            </x-ordering-selector>
-            @else
-            <x-ordering-selector>
-                {{ __('Paciente') }}
-            </x-ordering-selector>
-            @endif
-        </th>
         <th>
             <x-ordering-selector>
                 {{ __('Data da avaliação') }}
@@ -36,14 +25,6 @@ if(Auth::user()->isPatient()){
     <tbody>
         @foreach ($evaluations as $evaluation)
         <tr class="border transition delay-150 hover:bg-gray-100 text-left" x-data="{ confirm:false }">
-
-            <td class="pl-2 rounded-tl-sm">
-                @if(Auth::user()->isPatient())
-                {{ $evaluation->nutritionist->user->name }}
-                @else
-                {{ $evaluation->patient->user->name }}
-                @endif
-            </td>
             <td class="pl-3">
                 {{ explode(' ', $evaluation->created_at)[0] }}
             </td>
@@ -52,12 +33,15 @@ if(Auth::user()->isPatient()){
             </td>
             <td class="flex items-center justify-center gap-4 rounded-tr-sm">
                 <x-button-visual />
-                <x-button-edit />
-                <x-button-delete @click="confirm=true" />
+                @if(Auth::user()->isNutritionist())
+                    <x-button-edit href="{{ route('edit_evaluation', $evaluation) }}"/>
+                    <x-button-delete @click="confirm=true" />
+                @endif
                 <template x-if="confirm">
                     <x-modal>
                         <x-confirm-template>
-                            <x-confirm-button class="px-5">Sim</x-confirm-button>
+                            <x-confirm-button class="px-9">Sim</x-confirm-button>
+                            <x-slot name="text">{{ 'Você realmente deseja excluir a avaliação de '.explode(' ',$evaluation->patient->name)[0].'?' }}</x-slot>
                         </x-confirm-template>
                     </x-modal>
                 </template>
