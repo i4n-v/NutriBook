c436758c483a6b4c929910fc52928a9857a36ee2
i4n-v <ianvinicioss@gmail.com>
2021-07-10 18:04:56 -0300

    GH-41 -Adding anamnesis table to the page

---
+++ resources/views/components/anamnese-view-table.blade.php
@@ -0,0 +1,30 @@
+@props(['anamnese'])
+
+<table class="w-full bg-white shadow-lg border-solid border-b-2 rounded-sm" x-show="ana">
+    <thead thead class="h-10 bg-gray-700 text-white rounded-sm">
+        <th colspan="2" class="rounded-tl-sm rounded-tr-sm">Dobras cutâneas</th>
+        <th class="w-0"></th>
+    </thead>
+    <tbody>
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+            <td class="w-5/12 pl-2">Objetivo</td>
+            <td>{{ $anamnese->objective }}mm</td>
+        </tr>
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+            <td class="w-5/12 pl-2">Histórico patológico</td>
+            <td>{{ $anamnese->pathological_history }}cm</td>
+        </tr>
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+            <td class="w-5/12 pl-2">Histórico familiar</td>
+            <td>{{ $anamnese->family_history }}cm</td>
+        </tr>
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+            <td class="w-5/12 pl-2">Rémedios utilizados</td>
+            <td>{{ $anamnese->used_drugs }}cm</td>
+        </tr>
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+            <td class="w-5/12 pl-2">Estilo de vida</td>
+            <td>{{ $anamnese->life_style }}cm</td>
+        </tr>
+    </tbody>
+</table>
