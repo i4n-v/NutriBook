339853b4c35e44b5057f83a4a89e487f235eb2b6
i4n-v <ianvinicioss@gmail.com>
2021-07-10 15:46:42 -0300

    GH-44 -Adding anamnese data to the form for your editing

--- resources/views/components/anamnese-form.blade.php
+++ resources/views/components/anamnese-form.blade.php
@@ -1,3 +1,5 @@
+@props(['anamnese'])
+
 <form method="POST" action="{{ route('create_anamnese')}}" x-show="ana">
     @csrf
     <fieldset>
@@ -7,35 +9,35 @@
 
         <div>
             <x-label for="objective" :value="__('Objetivo')" />
-            <x-input id="objective" class="block mt-1 w-full" type="text" name="objective" required autofocus placeholder="Objetivo do paciente" />
+            <x-input id="objective" class="block mt-1 w-full" type="text" name="objective" :value="$anamnese->objective" required autofocus placeholder="Objetivo do paciente" />
         </div>
 
         <!--pathological_history -->
 
         <div>
             <x-label for="pathological_history" :value="__('Histórico Patológico')" />
-            <x-input id="pathological_history" class="block mt-1 w-full" type="text" name="pathological_history" required autofocus placeholder="Histórico Patológico do paciente" />
+            <x-input id="pathological_history" class="block mt-1 w-full" type="text" name="pathological_history" :value="$anamnese->pathological_history" required autofocus placeholder="Histórico Patológico do paciente" />
         </div>
 
         <!--family_history -->
 
         <div>
             <x-label for="family_history" :value="__('Histórico Familiar')" />
-            <x-input id="family_history" class="block mt-1 w-full" type="text" name="family_history" required autofocus placeholder="Histórico familiar do paciente" />
+            <x-input id="family_history" class="block mt-1 w-full" type="text" name="family_history" :value="$anamnese->family_history" required autofocus placeholder="Histórico familiar do paciente" />
         </div>
 
         <!--used_drugs -->
 
         <div>
             <x-label for="used_drugs" :value="__('Uso de Remédios')" />
-            <x-input id="used_drugs" class="block mt-1 w-full" type="text" name="used_drugs" required autofocus placeholder="Rémedios utilizados pelo paciente" />
+            <x-input id="used_drugs" class="block mt-1 w-full" type="text" name="used_drugs" :value="$anamnese->used_drugs" required autofocus placeholder="Rémedios utilizados pelo paciente" />
         </div>
 
         <!--life_style -->
 
         <div>
             <x-label for="life_style" :value="__('Estilo de Vida')" />
-            <x-input id="life_style" class="block mt-1 w-full" type="text" name="life_style" required autofocus placeholder="Estilo de Vida do paciente" />
+            <x-input id="life_style" class="block mt-1 w-full" type="text" name="life_style" :value="$anamnese->life_style" required autofocus placeholder="Estilo de Vida do paciente" />
         </div>
     </fieldset>
 
