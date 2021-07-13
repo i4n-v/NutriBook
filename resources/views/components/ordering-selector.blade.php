463d07fe82940de45d9b1a5e10a44638f2c7aeaa
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-07-13 09:56:55 -0300

    GH-32 and GH-33 Adding links and props for ordering and making some adjustments to the component

--- resources/views/components/ordering-selector.blade.php
+++ resources/views/components/ordering-selector.blade.php
@@ -1,17 +1,18 @@
-<div class="grid grid-cols-12">
-    <div class="col-start-1 col-end-10 m-auto">
-        {{ $slot }}
-    </div>
-    <div class="col-start-11 col-end-12">
-        <a href="">
+@props(['asc', 'desc'])
+
+<div class="space-y-1">
+    <span class="block hover:text-yellow-400">
+        <a href="{{ route('ordering', ['order' => $asc ?? '']) }}" class="">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 15l7-7 7 7" />
             </svg>
         </a>
-        <a href="">
+    </span>
+    <span class="block hover:text-yellow-400">
+        <a href="{{ route('ordering', ['order' => $desc ?? '']) }}">
             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
             </svg>
         </a>
-    </div>
+    </span>
 </div>
