ac625ec45f1786ff20f42d21c7a4c5bd5c75ead4
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-07-13 10:00:19 -0300

    GH-32 Preparing patients table to receive the ordering mechanism

--- resources/views/components/table-my-patients.blade.php
+++ resources/views/components/table-my-patients.blade.php
@@ -1,53 +1,88 @@
+@props(['column', 'value'])
+
 @php
 
-$nutritionist_collection = App\Models\Nutritionist::where('user_id', Auth::user()->id)->get();
+$nutritionist = App\Models\Nutritionist::where('user_id', Auth::user()->id)->get();
+$nutritionist_id = $nutritionist[0]->id;
+$eating_plans_patients = array();
+$users_patients = array();
 
 @endphp
 
 <table class="w-full bg-white shadow-lg border-solid border-b-2 rounded-sm">
     <thead class="h-10 bg-gray-700 text-white rounded-sm">
         <th class="rounded-tl-sm">
-            <x-ordering-selector>
-                {{ __('Nome') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    Nome
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="'nameAsc'" :desc="'nameDesc'"/>
+                </div>
+            </div>
         </th>
         <th>
-            <x-ordering-selector>
-                {{ __('CPF') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    CPF
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="'cpfAsc'" :desc="'cpfDesc'"/>
+                </div>
+            </div>
         </th>
         <th>
-            <x-ordering-selector>
-                {{ __('Data de vínculo') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    Data de vínculo
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="''" :desc="''"/>
+                </div>
+            </div>
         </th>
         <th class="rounded-tr-sm">Perfil</th>
     </thead>
 
-    @foreach ($nutritionist_collection as $nutritionist)
-        @foreach ($nutritionist->patient as $patient)
-            @php
-            $user_collection = App\Models\User::where('id', $patient->user_id)->get();
-            @endphp
+    @php
 
-            @foreach ($user_collection as $user) 
-            <tr class="border transition delay-150 hover:bg-gray-100 text-left">
-                <td class="pl-2 rounded-tl-sm">
-                    {{ $user->name }}
-                </td>
-                <td class="text-left">
-                    {{ $user->CPF }}
-                </td>
-                <td class="text-left">
-                    28/06/2021 (Em breve)
-                </td>
-                <td class="text-center w-2/12 rounded-tr-sm">
-                    <x-button-visual href="#"/>
-                </td>
-            </tr>
-            @endforeach
+    foreach (App\Models\EatingPlan::where('nutritionist_id', $nutritionist_id)->get() as $register) {
+        array_push($eating_plans_patients, $register->patient_id);
+    }
 
-        @endforeach
+    $my_patients = array_unique($eating_plans_patients);
+
+    foreach ($my_patients as $patient_id) {
+        foreach (App\Models\Patient::where('id', $patient_id)->get() as $patient) {
+            foreach (App\Models\User::where('id', $patient->user_id)->get() as $users) {
+                array_push($users_patients, $users->id);
+            }
+        }
+    }
+
+    if ($column == '' || $value == '') {
+        $patient_query = App\Models\User::whereIn('id', $users_patients)->orderBy('name', 'asc')->get();
+    } else {
+        $patient_query = App\Models\User::whereIn('id', $users_patients)->orderBy($column, $value)->get();
+    }
+
+    @endphp
+
+    @foreach ($patient_query as $user) 
+    <tr class="border transition delay-150 hover:bg-gray-100 text-left">
+        <td class="pl-2 rounded-tl-sm">
+            {{ $user->name }}
+        </td>
+        <td class="text-center">
+            {{ $user->CPF }}
+        </td>
+        <td class="text-center">
+            28/06/2021 (Em breve)
+        </td>
+        <td class="text-center w-2/12 rounded-tr-sm">
+            <x-button-visual href="#"/>
+        </td>
+    </tr>    
     @endforeach
 
 </table>
