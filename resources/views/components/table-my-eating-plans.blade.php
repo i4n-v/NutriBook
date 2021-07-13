34b17d6657055bfaa1eb431479a1be90050fe34d
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-07-13 09:59:41 -0300

    GH-33 Preparing eating plans table to receive the ordering mechanism

--- resources/views/components/table-my-eating-plans.blade.php
+++ resources/views/components/table-my-eating-plans.blade.php
@@ -1,3 +1,5 @@
+@props(['column', 'value'])
+
 @php
 
 $patient_collection = App\Models\Patient::where('user_id', Auth::user()->id)->get();
@@ -7,56 +9,74 @@ $patient_collection = App\Models\Patient::where('user_id', Auth::user()->id)->ge
 <table class="w-full bg-white shadow-lg border-solid border-b-2 rounded-sm">
     <thead class="h-10 bg-gray-700 text-white rounded-sm">
         <th class="rounded-tl-sm">
-            <x-ordering-selector>
-                {{ __('Título') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    Título
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="'titleAsc'" :desc="'titleDesc'"/>
+                </div>
+            </div>
         </th>
         <th>
-            <x-ordering-selector>
-                {{ __('Data de início') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    Data de início
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="'dateStartAsc'" :desc="'dateStartDesc'"/>
+                </div>
+            </div>
         </th>
         <th>
-            <x-ordering-selector>
-                {{ __('Data de término') }}
-            </x-ordering-selector>
+            <div class="grid grid-cols-12">
+                <div class="col-start-1 col-end-12 m-auto">
+                    Data de término
+                </div>
+                <div class="col-start-12 col-end-12">
+                    <x-ordering-selector :asc="'dateFinishAsc'" :desc="'dateFinishDesc'"/>
+                </div>
+            </div>
         </th>
         <th class="rounded-tr-sm">Ações</th>
     </thead>
 
     @foreach ($patient_collection as $patient)
-    @php
-    $eating_plan_collection = App\Models\EatingPlan::where('patient_id', $patient->id)->get();
+        
+        @php
+        if ($column == '' || $value == '') {
+            $eating_plan_collection = App\Models\EatingPlan::where('patient_id', $patient->id)->orderBy('title', 'asc')->get();
+        } else {
+            $eating_plan_collection = App\Models\EatingPlan::where('patient_id', $patient->id)->orderBy($column, $value)->get();
+        }
+        @endphp
 
-    @endphp
-
-    @foreach ($eating_plan_collection as $eating_plan)
-    <tr class="border transition delay-150 hover:bg-gray-100 text-left" x-data="{ confirm:false }">
-
-        <td class="pl-2 rounded-tl-sm">
-            {{ $eating_plan->title }}
-        </td>
-        <td>
-            {{ $eating_plan->date_start }}
-        </td>
-        <td>
-            {{ $eating_plan->date_finish }}
-        </td>
-        <td class="flex items-center justify-center gap-4 rounded-tr-sm">
-            <x-button-visual href="{{ route('view_eating_plan', $eating_plan) }}" />
-            <x-button-edit />
-            <x-button-delete @click="confirm=true" />
-            <template x-if="confirm">
-                <x-modal>
-                    <x-confirm-template>
-                        <x-confirm-button class="px-9" href="{{ route('eatingplan_delete', $eating_plan->id) }}">Sim</x-confirm-button>
-                        <x-slot name="text">Você realmente deseja excluir este plano alimentar?</x-slot>
-                    </x-confirm-template>
-                </x-modal>
-            </template>
-        </td>
-    </tr>
-    @endforeach
+        @foreach ($eating_plan_collection as $eating_plan)
+        <tr class="border transition delay-150 hover:bg-gray-100 text-left" x-data="{ confirm:false }">
+    
+            <td class="pl-2 rounded-tl-sm">
+                {{ $eating_plan->title }}
+            <td class="text-center">
+                {{ $eating_plan->date_start }}
+            </td>
+            <td class="text-center">
+                {{ $eating_plan->date_finish }}
+            </td>
+            <td class="flex items-center justify-center gap-4 rounded-tr-sm">
+                <x-button-visual href="{{ route('view_eating_plan', $eating_plan) }}" />
+                <x-button-edit />
+                <x-button-delete @click="confirm=true" />
+                <template x-if="confirm">
+                    <x-modal>
+                        <x-confirm-template>
+                        <x-confirm-button class="px-9" href="{{ route('eatingplan_delete', $eating_plan->id) }}">   Sim</x-confirm-button>
+                            <x-slot name="text">Você realmente deseja excluir este plano alimentar?</x-slot>
+                        </x-confirm-template>
+                    </x-modal>
+                </template>
+            </td>
+        </tr>
+        @endforeach
 
     @endforeach
 
