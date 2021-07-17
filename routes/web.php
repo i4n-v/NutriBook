2d0a444f08a6add421c463c9eed14a2e004513c2
bismarckcesar <bismarcksport@gmail.com>
2021-07-17 15:01:14 -0300

    GH-48 -adding routes for email validation and access to the nutribook system

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\EvaluationController;
 use App\Http\Controllers\EatingPlanController;
 use App\Http\Controllers\OrderingSelectorController;
 use App\Http\Controllers\UserController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 
 /*
@@ -22,13 +24,33 @@ use App\Http\Controllers\UserController;
 |
 */
 
+
+Route::get('/email/verify', function () {
+    return view('auth.verify-email');
+})->middleware('auth')->name('verification.notice');
+
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill();
+
+    return redirect('/home');
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+
 Route::get('/', function () {
     return redirect('/login');
-});
+});    
+
+// Route::middleware([])
+
 
 Route::get('/home', function () {
     return view('home');
-})->middleware(['auth'])->name('home');
+})->middleware(['auth', 'verified'])->name('home');
 
 //profile routes
 
