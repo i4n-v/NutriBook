0fb115f5d842fd31d521fe16a7ce2d5756d94f10
alowce <alicebrandt.c@gmail.com>
2021-07-20 12:46:43 -0300

    GH-32 Adding component to patient filter

---
+++ resources/views/components/filters-patients.blade.php
@@ -0,0 +1,11 @@
+<div class="">
+<form action="">
+	<label for="">Nome do paciente:</label>
+	<x-input type="text"/>
+	<label for="">CPF:</label>
+	<x-input type="text"/>
+	<label for="">Data de vínculo:</label>
+	<x-input type="date"/>
+	<x-button>Filtrar</x-button>
+</form>
+</div>
