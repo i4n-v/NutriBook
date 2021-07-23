207737e0fbb96d5efc6f1233f16e42e6d461f4c1
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-07-23 16:47:48 -0300

    GH-50 Adding patient controller

---
+++ app/Http/Controllers/PatientController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PatientController extends Controller
+{
+    public function nutriPatients() {
+        $nutri = Auth::user()->nutritionistProfile;
+        $nutri_patients = $nutri->eatingPlans->map(function($ep) {
+            return $ep->patient->user->id;
+        });
+        $patient_query = \App\Models\User::with('patientProfile')->whereIn('id', $nutri_patients)->orderBy('name', 'asc')->get()->append('formattedCPF');
+        return $patient_query->toJson();
+    }
+}
