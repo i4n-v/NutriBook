eb09643a2ef7aace2c34d12644e8936302a1d8af
i4n-v <ianvinicioss@gmail.com>
2021-08-04 18:24:02 -0300

    GH-52 -eating plan visualization modifications

--- resources/views/components/eating-plans-view.blade.php
+++ resources/views/components/eating-plans-view.blade.php
@@ -2,6 +2,8 @@
     $name = explode(' ', $eatingPlan->patient->user->name);
     $name = "$name[0] $name[1]";
     $user = $eatingPlan->patient->user;
+
+    $meals = $eatingPlan->meals->all();
 @endphp
 <x-guest-layout>
     <div class="min-h-screen bg-white">
@@ -14,16 +16,14 @@
                 @else
                     <a href="'{{ route('home') }}'" class="transition delay-150 hover:text-gray-900">Planos alimentares</a> >
                 @endif
-                <span class="cursor-default">Plano alimentar</span>
+                <span class="cursor-default">{{ $eatingPlan->title }}</span>
             </div>
         </x-slot>
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mt-5">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 bg-gray-100 border-b border-gray-200" x-bind:class="food?'':'bg-gray-800 w-10/12 rounded-lg m-auto'">
-                        <div class="flex gap-4">
-
-                            chegou mzr
+                    <div class="bg-gray-100 rounded-lg m-auto p-6" x-bind:class="food?'':'bg-gray-800 w-10/12 rounded-lg m-auto'">
+                        <div class="text-gray-900">
 
                         </div>
                     </div>
