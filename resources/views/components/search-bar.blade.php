087921d4e9fa9ae764689ee739b1ffdf3a1fbf9c
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-08-04 05:39:11 -0300

    GH-54 Modifying search component to fit current layout

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -1,8 +1,27 @@
-<div class="h-1/6 relative shadow-md mb-2 border-none">
-    <input class="text-white bg-transparent rounded-full w-full border-2 border-yellow-500 py-2 leading-tight focus:outline-none shadow-sm focus:border-yellow-500 focus:ring-1 focus:ring-yellow-500 placeholder-white" type="text" placeholder="{{ $placeholder ?? '' }}"/>
-    <div class="absolute top-0.5 bottom-0.5 right-0.75 cursor-pointer border p-1 rounded-full bg-white transition delay-150">   
-        <button class="border-none focus:outline-none">
-            <x-search-icon/>
-        </button>
-    </div>
+<div x-data="users" x-init="loadUsers" x-on:click="showUsers = true" x-on:click.away="showUsers = false" x-bind:class="{ 'border-2 border-yellow-300 rounded-sm': showUsers }" class="w-full max-w-lg font-medium">	
+	<form x-bind:class="{ 'bg-white': showUsers }" class="border-none" >
+  		<div class="flex items-center h-10 border-b-2 border-gray-400 py-2">
+    		<input id="searchInput" x-bind:class="{ 'pb-4': showUsers }" class="teste appearance-none bg-transparent border-none border-2 rounded-sm w-full text-black placeholder-gray-400 mr-3 pb-1 pt-4 pl-1 pr-2 leading-none outline-none" type="text" placeholder="{{ $placeholder ?? '' }}" aria-label="Full name" autocomplete="off">
+	    	<span x-bind:class="{ 'pb-2': showUsers }" class="pt-2 cursor-pointer pr-0.5">
+				<x-search-icon/>
+			</span>
+  		</div>
+	</form>
+	<template x-if="showUsers">
+		<div class="bg-white text-black duration-300">
+            <template x-for="user in users">
+			    <div class="w-full grid grid-cols-3 gap-2 border-t-2 border-solid py-0.5 px-1">
+			    	<div class="col-start-1 col-span-10 pt-1.5" x-text="user.name"></div>
+			    	<div class="col-start-11 col-span-1 pt-0.5">
+			    		<x-button-visual x-bind:href="'/profile/' + user.id"/>
+			    	</div>
+			    	<div class="col-start-12 col-span-1 pt-2">
+			    		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+			    			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
+			    		</svg>
+			    	</div>
+			    </div>
+            </template>
+        </div>
+    </template>
 </div>
