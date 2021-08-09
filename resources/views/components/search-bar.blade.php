83871fd537e5b5fe9c6e3c13b2ad5d95622ca281
FabioMouradeFraga <fabiomouradefraga@gmail.com>
2021-08-09 13:51:55 -0300

    GH-54 Removing element that would be used to request link

--- resources/views/components/search-bar.blade.php
+++ resources/views/components/search-bar.blade.php
@@ -15,11 +15,13 @@
 			    	<div class="col-start-11 col-span-1 pt-0.5">
 			    		<x-button-visual x-bind:href="'/profile/' + user.id"/>
 			    	</div>
-			    	<div class="col-start-12 col-span-1 pt-2">
+					{{--
+					<div class="col-start-12 col-span-1 pt-2">
 			    		<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 			    			<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" />
 			    		</svg>
 			    	</div>
+					--}}
 			    </div>
             </template>
         </div>
